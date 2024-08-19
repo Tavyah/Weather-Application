7dac5a68ce17bfee07eb43028842c73284f9b07e
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-08-19 13:30:30 +0200

    Updated swedish api to write a list with locations in json format

--- sverige.py
+++ sverige.py
@@ -1,10 +1,11 @@
-import requests, os, configparser
-import pandas as pd
+import requests, os
 from typing import Callable
+import json
+import pandas as pd
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_OUTPUT = '/data/'
-FILENAME_OUTPUT_SWEDEN = 'weather-output_sweden.json'
+FILENAME_OUTPUT_SWEDEN = 'weather_output_smhi.json'
 
 geo_locations = {
     "Oslo": (59.9, 17.6),
@@ -16,8 +17,8 @@ def _fetch_weather_api_sweden(lat: float, lon: float) -> str:
         
 def _get_requests_api_weather(api_call: Callable) -> requests:
     r = requests.get(api_call)
-    if r == 200:
-        return r
+    if r.status_code == 200:
+        return r.json()
     else:
         return f'Error: Recieved status code {r.status_code}'
 
@@ -26,8 +27,9 @@ def write_weather_log():
     for city in geo_locations:
         (lat, lon) = geo_locations[city]
         locations.append(_get_requests_api_weather(_fetch_weather_api_sweden(lat, lon)))
-
-    weather_df = pd.DataFrame(locations)
-    weather_df.to_json(CURR_DIR_PATH + DATA_OUTPUT + FILENAME_OUTPUT_SWEDEN, index=False)
+        
+    with open(CURR_DIR_PATH + DATA_OUTPUT + FILENAME_OUTPUT_SWEDEN, 'w') as file:
+        file.write(json.dumps(locations))
+    file.close()
 
 write_weather_log()
