c12e7ddd051346a11a239be311589bd54b855650
Tiril Marie Pedersen Løken <94623622+Tavyah@users.noreply.github.com>
2024-08-19 11:28:34 +0200

    added swedish api

--- sverige.py
+++ sverige.py
@@ -3,22 +3,13 @@ import pandas as pd
 import json
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_OUTPUT = '/data/'
+FILENAME_OUTPUT_SWEDEN = 'weather-output_sweden.json'
 
-def _write_weather_log():
-
-    r = requests.get('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.9&lon=10.8')
-
-    weather_df = r.json()
-    weather_df = pd.DataFrame(weather_df)
-    weather_df.to_json(CURR_DIR_PATH + '/data/weather-output.json', index=False)
-
-_write_weather_log()
-
-def _write_weather_log2():
-
-
-    weather_df = pd.DataFrame(r)
-    weather_df.to_json(CURR_DIR_PATH + '/weather-output.json', index=False)
+geo_locations = {
+    "Oslo": (59.9, 17.6),
+    "Stockholm": (43, 34.32)
+}
 
 def _fetch_weather_api_sweden(lat: float, lon: float) -> str:
     return f'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{lon}/lat/{lat}/data.json'
@@ -26,7 +17,13 @@ def _fetch_weather_api_sweden(lat: float, lon: float) -> str:
 def _get_requests_api_weather(api_call: function) -> requests:
     return requests.get(api_call)
 
+def write_weather_log():
+    locations = []
+    for city in geo_locations:
+        (lat, lon) = geo_locations[city]
+        locations.append(_get_requests_api_weather(_fetch_weather_api_sweden(lat, lon)))
 
-    
+    weather_df = pd.DataFrame(locations)
+    weather_df.to_json(CURR_DIR_PATH + DATA_OUTPUT + FILENAME_OUTPUT_SWEDEN, index=False)
 
-
+write_weather_log()
